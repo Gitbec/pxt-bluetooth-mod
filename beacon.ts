c0d8aaa7047380d75fede0a5e3c9f73dc5e184b5
Gitbec <45407940+Gitbec@users.noreply.github.com>
2024-01-22 18:08:17 +0100

    Update beacon.ts

--- beacon.ts
+++ beacon.ts
@@ -12,34 +12,15 @@ namespace bluetooth_senden{
      * Prints a numeric value to the serial
      */
     //% help=bluetooth/uart-write-number weight=79
-    //% weight=89 blockGap=8 advanced=true
+    //% weight=89 blockGap=8 
     //% blockId=bluetooth_uart_writenumber block="bluetooth uart|write number %value"
     export function uartWriteNumber(value: number): void {
         uartWriteString(value.toString());
     }
     
-    //% blockId=beacon_eddystone_advertise_uid block="bluetooth mod UID|namespace (bytes 6-9)%ns|instance (bytes 2-6)%instance|with power %power|connectable %connectable"
-    //% parts=bluetooth weight=12 blockGap=8
-    //% help=bluetooth/advertise-uid blockExternalInputs=1
-    export function advertiseUid(ns: number, instance: number, power: number, connectable: boolean) {
-        const buf = pins.createBuffer(16);
-        buf.setNumber(NumberFormat.Int32BE, 6, ns);
-        buf.setNumber(NumberFormat.Int32BE, 12, instance);
-        bluetooth.advertiseUidBuffer(buf, power, connectable);
-    }
+   
 
-    /**
-        * Advertise an Eddystone URL
-        * @param url the url to transmit. Must be no longer than the supported eddystone url length, eg: "https://makecode.com"
-        * @param power power level between 0 and 7, eg: 7
-        * @param connectable true to keep bluetooth connectable for other services, false otherwise.
-        */
-    //% blockId=beacon_eddystone_advertise_url block="bluetooth mod url %url|with power %power|connectable %connectable"
-    //% parts=bluetooth weight=11 blockGap=8
-    //% help=bluetooth/advertise-url blockExternalInputs=1
-    export function advertiseUrl(url: string, power: number, connectable: boolean) {
-        bluetooth.advertiseUrl(url, power, connectable)
-    }
+   
 
     /**
     * Stops advertising Eddystone end points
