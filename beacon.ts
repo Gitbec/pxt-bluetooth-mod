4ecbeef1094a673c705b5997ebaf2791abbfbe3b
Gitbec <45407940+Gitbec@users.noreply.github.com>
2024-01-22 17:00:11 +0100

    Update beacon.ts

--- beacon.ts
+++ beacon.ts
@@ -8,7 +8,7 @@ namespace beacon {
   * @param power power level between 0 and 7, eg: 7
   * @param connectable true to keep bluetooth connectable for other services, false otherwise.
   */
-    //% blockId=beacon_eddystone_advertise_uid block="bluetooth advertise UID|namespace (bytes 6-9)%ns|instance (bytes 2-6)%instance|with power %power|connectable %connectable"
+    //% blockId=beacon_eddystone_advertise_uid block="bluetooth mod UID|namespace (bytes 6-9)%ns|instance (bytes 2-6)%instance|with power %power|connectable %connectable"
     //% parts=bluetooth weight=12 blockGap=8
     //% help=bluetooth/advertise-uid blockExternalInputs=1
     export function advertiseUid(ns: number, instance: number, power: number, connectable: boolean) {
